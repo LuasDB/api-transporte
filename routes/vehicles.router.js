99b65535ea659914841074f95d21c0f279cd8d3c
Marco Romaniz <sti@siradiacion.com.mx>
2024-06-04 21:36:47 -0600

    actualizacion

--- routes/vehicles.router.js
+++ routes/vehicles.router.js
@@ -1,18 +1,27 @@
 // //Llamado de express
-// const express = require('express')
+const express = require('express')
 // //Metodo routet de express
-// const router = express.Router();
+const router = express.Router();
 // //dimportacion del servicio trucks
-// const Vehicle = require('../services/vehicles.service.js');
-// //creo nueva instancia para el truck
-// const vehicle = new Vehicle();
-// //creacion de endpoint
-// router.get('/',async(req,res)=>{
-//     //Obtener todos los los trucks
-//     const trucks =await truck.getAll();
-//     res.status(200).json(trucks);
+const Vehicle = require('../services/vehicles.service.js');
+//importar multer
+const multer = require('multer');
 
-// })
+// //creo nueva instancia para el truck
+const upLoadVehicle = multer();
+
+const vehicle = new Vehicle();
+// //creacion de endpoint
+router.get('/',async(req,res,next)=>{
+//     //Obtener todos los los trucks
+try{
+
+}catch (error){
+    next(error);
+}
+
+})
+
 // router.get('/:IDtruck',async(req,res)=>{
 //     const ID = req.params.IDtruck;
 //     const getTruck = await truck.getOne(ID);
